7ee5d722f14834c17865259d3ac0a52317271409
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-07-11 23:47:16 +0900

    Effect

--- web-movieservice/src/App.js
+++ web-movieservice/src/App.js
@@ -2,10 +2,18 @@ import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev ) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue"/>
+      <div>
+        <h1>{counter}</h1>
+        <button onCLick={onClick}>Click me</button>
+      </div>
+      <div>
+        <h1 className={styles.title}>Welcome back!</h1>
+        <Button text="Continue"/>
+      </div>
     </div>
   );
 }
