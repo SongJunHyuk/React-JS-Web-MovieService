c8cd1cb7f1141846ca98281d90de7a86c7dfe1d6
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-07-21 23:36:46 +0900

    Practice Movie App

--- web-movieservice/src/App.js
+++ web-movieservice/src/App.js
@@ -25,6 +25,20 @@ const App = () => {
       });
   }, []);
 
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -51,6 +65,21 @@ const App = () => {
           ))}
         </select>
       )}
+
+      <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
