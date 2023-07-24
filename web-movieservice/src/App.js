f09c3e5cfbe4c98747d3e67da716a6becb2fde41
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-07-24 23:31:24 +0900

    Practice Movie App

--- web-movieservice/src/App.js
+++ web-movieservice/src/App.js
@@ -82,10 +82,10 @@ const App = () => {
       </div>
       <Router>
       <Switch>
-        <Route path="/hello">
+        <Route path="/abot-us">
           <h1>Hello</h1>
         </Route>
-        <Route path="/movie">
+        <Route path="/movie/:id">
           <Detail />
         </Route>
         <Route path="/">
